3f6e7837e5ef187f547689eab46cd0d0e9a298e9
IrinaTarasova <63746987+IrinaTarasova98@users.noreply.github.com>
2021-09-13 08:15:46 +0500

    Update Apartment.java

--- src/main/java/com/example/demo/model/Apartment.java
+++ src/main/java/com/example/demo/model/Apartment.java
@@ -27,53 +27,50 @@ public class Apartment {
 	// номер квартиры
 	public int id;
 	// параметры
-    int values[];
+	int values[];
 
-    // конструктор
-    public Apartment(int id)
-    {
-    	values = new int[] {0, 20, 0, 20, 0, 20, 0, 20, 0};
-    	this.id = id;
-    }
+	// конструктор
+	public Apartment(int id)
+	{
+		values = new int[] {0, 20, 0, 20, 0, 20, 0, 20, 0};		
+		this.id = id;
+	}
         
-    // получить параметры квартиры в массив
-    public int[] getValues()
-    {
+	// получить параметры квартиры в массив
+	public int[] getValues()
+	{
 		try {
 			// открыть файл для чтения
 			Scanner scanner = new Scanner(new File("src/main/resources/appartments/" + this.id));
 			// получить все значения
 			int i = 0;
 			while (scanner.hasNext()) {
-					values[i++] = scanner.nextInt();
+				values[i++] = scanner.nextInt();
 			}
 			scanner.close();
 		}
-        catch(IOException ex){
-        	System.out.println(ex.getMessage());
-        } 
-    	return values;
-    }    
-    
-    public void setValues(int[] set) {
-    	values = set;
-    }
-    
-    // сохранить параметры в файл
-    public void writeValues() {
-        try(FileWriter writer = new FileWriter("src/main/resources/appartments/" + this.id, false))
-        {
-        	for(int i = 0; i < 9; i++) {
-	            writer.write(Integer.toString(values[i]));
-	            writer.append(System.lineSeparator());
+        	catch(IOException ex){
+        		System.out.println(ex.getMessage());
         	} 
-            writer.close();
-        }
-        catch(IOException ex){
-             
-            System.out.println(ex.getMessage());
-        } 
-    	
-    }
-        
+    		return values;
+	}    
+    
+	public void setValues(int[] set) {
+		values = set;
+	}
+    
+	// сохранить параметры в файл
+	public void writeValues() {
+		try(FileWriter writer = new FileWriter("src/main/resources/appartments/" + this.id, false))
+		{
+			for(int i = 0; i < 9; i++) {
+				writer.write(Integer.toString(values[i]));
+				writer.append(System.lineSeparator());
+			} 
+			writer.close();
+		}
+		catch(IOException ex){     
+			System.out.println(ex.getMessage());
+		} 
+	}
 }
